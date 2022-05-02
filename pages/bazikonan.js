b6c3c657f4540ef831a14d65ce239b6867d5ff93
abdollahchelasi <abdollah.chelasi@gmail.com>
2022-05-02 14:09:25 +0430

    new Bazikonan

--- pages/bazikonan.js
+++ pages/bazikonan.js
@@ -1,37 +1,42 @@
 import Head from "next/head";
+import Link from "next/link";
 import { Col, Container, Row } from "react-bootstrap";
-import styles from '../styles/Home.module.css'
-
+import styles from "../styles/Home.module.css";
 
 function bazikonan() {
-    return (
-        <div>
-            <Head>
-                <title>ویدیوهای ورزشی گربدان</title>
-            </Head>
-            <main>
-                <Container className={styles.contmain}>
-                    <Row>
-                        <Col sm={12} className={styles.titlevid}>بازیکنان ورزشی</Col>
-                        <Col sm={12} className={styles.videoo}>
-                                <Row >
-                                    <Col className={styles.vidcoo1}>
-                                        <img className={styles.img1} src="images/passdolfin.jpg"/>
-                                    </Col>
-                                    <Col className={styles.vidcoo2}>
-                                    <img className={styles.img1} src="images/passdolfin.jpg"/>
+  return (
+    <div>
+      <Head>
+        <title>ویدیوهای ورزشی گربدان</title>
+      </Head>
+      <main>
+        <Container className={styles.contmain}>
+          <Row>
+            <Col sm={12} className={styles.titlevid}>
+              بازیکنان ورزشی
+            </Col>
+            <Col sm={12} className={styles.videoo}>
+              <Row>
+                <Col className={styles.vidcoo1}>
+                  <Link href="images/passdolfin.jpg">
+                    <img className={styles.img1} src="images/passdolfin.jpg" />
+                  </Link>
+                </Col>
 
-                                    </Col>
-                                </Row>
-                        </Col>
+                <Col className={styles.vidcoo2}>
+                  <Link href="images/passdolfin.jpg">
+                    <img className={styles.img1} src="images/passdolfin.jpg" />
+                  </Link>
+                </Col>
+                
 
-                        
-                        
-                    </Row>
-                </Container>
-            </main>
-        </div>
-    );
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+    </div>
+  );
 }
 
 export default bazikonan;
