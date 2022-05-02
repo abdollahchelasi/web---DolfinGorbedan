22c762347f7af8ccf5941a7c4200905a7d4e409c
abdollahchelasi <abdollah.chelasi@gmail.com>
2022-05-02 22:40:06 +0430

    new

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { Container , Col,Row } from "react-bootstrap";
 import styles from "../styles/Home.module.css";
 import Navb from "./Navb";
 
@@ -9,7 +10,24 @@ const Layout = ({ children }) => {
       
         <div>{children}</div>
 
-      <footer className={styles.footerr}>Powered By Dolfin Gorbedan</footer>
+      <footer className={styles.footerr}>
+
+        <Container >
+          <Row>
+            <Col sm={12} >
+              <img src="/images/dolfin.png" width={70}/>
+              <p>باشگاه فرهنگی ورزشی دلفین گربدان</p>
+            </Col>
+            
+            
+              
+            <Col sm={12} className={styles.abdollah}>
+              این سایت توسط <span className={styles.abdol}>عبدالله چلاسی <img src="/images/a.jpg" className={styles.imabdol} width={20}/></span>ساخته شده است
+            </Col>
+          </Row>
+        </Container>
+
+      </footer>
     </div>
   );
 };
